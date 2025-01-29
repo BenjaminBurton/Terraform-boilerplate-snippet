51ad7dd0c90f2ce2a8d0d80e87665cc12209e426
Mr. Burton <benjaminlburton@gmail.com>
2025-01-29 08:38:24 -0500

    initial commit with boilerplate snippet

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 1.0.4"
+    }
+  }
+}
+
+variable "aws_region" {}
+
+variable "base_cidr_block" {
+  description = "A /16 CIDR range definition, such as 10.1.0.0/16, that the VPC will use"
+  default = "10.1.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones in which to create subnets"
+  type = list(string)
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.base_cidr_block
+}
+
+resource "aws_subnet" "az" {
+  count = length(var.availability_zones)
+  availability_zone = var.availability_zones[count.index]
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index+1)
+}
+
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value = aws_vpc.main.id
+}
